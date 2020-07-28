8241084770314deb4243cf7ff42aaec53172bf04
auze217 <auze217@vt.edu>
2020-07-28 14:26:25 -0400

    adding sports component

--- dashboard-app/src/app/app.module.ts
+++ dashboard-app/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SportsComponent } from './sports/sports.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SportsComponent
   ],
   imports: [
     BrowserModule
